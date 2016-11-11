ef96c3f82762e37cf3a74438b489b4a08323532e
eashan <eashankadam@gmail.com>
2016-11-10 22:04:18 -0800

    Basic script

---
+++ setup-project.sh
@@ -0,0 +1,17 @@
+#This file will install git, python pip, setup a virtualenv, and download a git project as specified in the command line argument
+#Takes 2 arguments
+sudo apt-get install git
+sudo apt-get install Python-pip
+sudo apt-get install Python-dev
+
+sudo pip install virtualenv
+
+git clone $1
+
+cd $2
+
+virtualenv env
+source env/bin/activate
+
+pip install -r requirements.txt
+
